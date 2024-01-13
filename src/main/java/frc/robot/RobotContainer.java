6e3b3c7308529395130e068f6e1f9d240cc4d585
Tyler Seiford <50031864+TylerSeiford@users.noreply.github.com>
2024-01-13 11:24:03 -0600

    Add zero and X buttons

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,35 +7,41 @@ package frc.robot;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import frc.robot.constants.OIConstants;
 import frc.robot.subsystems.Bass;
 
 public class RobotContainer {
-  private final Bass m_robotDrive = new Bass();
-  private final CommandJoystick one = new CommandJoystick(0);
-  private final CommandJoystick two = new CommandJoystick(1);
+    private final Bass m_robotDrive = new Bass();
+    private final CommandJoystick one = new CommandJoystick(0);
+    private final CommandJoystick two = new CommandJoystick(1);
 
+    public RobotContainer() {
+        configureBindings();
 
-  public RobotContainer() {
-    configureBindings();
+        m_robotDrive.setDefaultCommand(
+                // The left stick controls translation of the robot.
+                // Turning is controlled by the X axis of the right stick.
+                new RunCommand(
+                        () -> m_robotDrive.drive(
+                                -MathUtil.applyDeadband(one.getY(), OIConstants.kDriveDeadband),
+                                -MathUtil.applyDeadband(one.getX(), OIConstants.kDriveDeadband),
+                                -MathUtil.applyDeadband(two.getX(), OIConstants.kDriveDeadband),
+                                true, true),
+                        m_robotDrive));
+    }
 
-      m_robotDrive.setDefaultCommand(
-        // The left stick controls translation of the robot.
-        // Turning is controlled by the X axis of the right stick.
-        new RunCommand(
-            () -> m_robotDrive.drive(
-                -MathUtil.applyDeadband(one.getY(), OIConstants.kDriveDeadband),
-                -MathUtil.applyDeadband(one.getX(), OIConstants.kDriveDeadband),
-                -MathUtil.applyDeadband(two.getX(), OIConstants.kDriveDeadband),
-                true, true),
-            m_robotDrive));
-  }
+    private void configureBindings() {
+        one.trigger().toggleOnTrue(new StartEndCommand(m_robotDrive::setX, () -> {
+        }, m_robotDrive));
 
-  private void configureBindings() {}
+        two.trigger().onTrue(new InstantCommand(() -> m_robotDrive.zeroHeading(), m_robotDrive));
+    }
 
-  public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
-  }  
+    public Command getAutonomousCommand() {
+        return Commands.print("No autonomous command configured");
+    }
 }
