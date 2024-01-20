6aa1e4a9004a1b1c320a0ad7ac29ff7aab67847e
Functian <ianhauer@icloud.com>
2024-01-20 11:04:33 -0600

    Deleted some unnecessary code

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,7 +1,6 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.math.geometry.Translation2d;
 
 import com.revrobotics.CANSparkBase.ControlType;
 import com.revrobotics.CANSparkBase.IdleMode;
@@ -11,21 +10,17 @@ import com.revrobotics.SparkPIDController;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.SparkAbsoluteEncoder.Type;
 
-import static frc.robot.Constants.*;
-
-import org.littletonrobotics.junction.Logger;
+import frc.robot.Constants.*;
 
 public class Arm extends SubsystemBase {
     private static int STALL_CURRENT_LIMIT_SHOULDER = 30;
     private static int FREE_CURRENT_LIMIT_SHOULDER = 25;
     private static int SECONDARY_CURRENT_LIMIT_SHOULDER = 35;
-
     private CANSparkMax shoulderMotorRight;
     private CANSparkMax shoulderMotorLeft;
     private static SparkAbsoluteEncoder shoulderEncoder;
     private SparkPIDController shoulderPID;
     private double shoulderSetpoint;
-    public double ARM_SHOULDER_LENGTH;
 
     public Arm() {
 
@@ -68,6 +63,7 @@ public class Arm extends SubsystemBase {
         return angle;
     }
 
+    //Angle setpoints will need to be different
     public void setShoulderSetpoint(double setpoint) {
         while (setpoint > 360) {
             setpoint -= 360;
@@ -75,52 +71,15 @@ public class Arm extends SubsystemBase {
         while (setpoint < 0) {
             setpoint += 360;
         }
-
         if (setpoint < 0 || setpoint > 280) {
-
             setpoint = 0;
         } else if (setpoint > 130 && setpoint < 280) {
-
             setpoint = 130;
-        } else {
-
         }
-        if (!violatesFramePerimeter( getShoulderAngle())) {
-            shoulderSetpoint = setpoint;
-
-        } else {
-            System.out.println("hit limit");
-        }
-    }
-
-    public double getShoulderSetpoint() {
-        return shoulderSetpoint;
-    }
-
-    public Translation2d getArmPosition(double shoulder_Angle) {
-        // 105 is when its 6 inches off
-
-        double shoulder_Compliment = 180 - shoulder_Angle;
-
-        Translation2d output = new Translation2d();
-        return output;
-    }
-
-    public boolean violatesFramePerimeter(double shoulder_Angle) {
-        double currentExtension = getArmPosition(getShoulderAngle()).getX();
-        double desiredExtension = getArmPosition(shoulder_Angle).getX();
-        return shoulder_Angle > 105 && !(desiredExtension < currentExtension || desiredExtension < 45);
-
     }
 
     @Override
     public void periodic() {
-        Logger.recordOutput("Arm/Shoulder/Angle", getShoulderAngle());
-        Logger.recordOutput("Arm/Shoulder/Setpoint", shoulderSetpoint);
-        Logger.recordOutput("Arm/Shoulder/LeftOutput", shoulderMotorRight.getAppliedOutput());
-        Logger.recordOutput("Arm/Shoulder/RightOutput", shoulderMotorLeft.getAppliedOutput());
-        Logger.recordOutput("Arm/X", getArmPosition(getShoulderAngle()).getX());
-
         shoulderPID.setReference(shoulderSetpoint, ControlType.kPosition);
     }
 }
