506e8e7eb00435657824b330618d42d8fae7393b
PineappleGirl5 <bigflatblackhole@gmail.com>
2024-01-20 14:07:33 -0600

    odometry test auto

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "FB2024";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 14;
+  public static final String GIT_SHA = "62818f60bde5b8901729245c2627bc8db878ad26";
+  public static final String GIT_DATE = "2024-01-20 11:15:32 EST";
+  public static final String GIT_BRANCH = "32-subsystem-15-prototype-arm";
+  public static final String BUILD_DATE = "2024-01-20 11:21:32 EST";
+  public static final long BUILD_UNIX_TIME = 1705767692825L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants(){}
+}
