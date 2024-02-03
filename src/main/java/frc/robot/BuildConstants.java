79a4e5bfde98ea425e0fae5de721a16f4061efd2
Functian <ianhauer@icloud.com>
2024-02-03 14:23:23 -0600

    added stuff

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FB2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 17;
-  public static final String GIT_SHA = "c49fcb311cfa736b5fc358e101ff7c66f82f831c";
-  public static final String GIT_DATE = "2024-01-20 15:19:02 EST";
+  public static final int GIT_REVISION = 19;
+  public static final String GIT_SHA = "872d09c839cbaf122cddff62e639728d78cc0039";
+  public static final String GIT_DATE = "2024-01-30 21:43:43 EST";
   public static final String GIT_BRANCH = "pathplannerAutos";
-  public static final String BUILD_DATE = "2024-01-20 15:38:58 EST";
-  public static final long BUILD_UNIX_TIME = 1705783138003L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2024-02-03 11:13:23 EST";
+  public static final long BUILD_UNIX_TIME = 1706976803795L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
