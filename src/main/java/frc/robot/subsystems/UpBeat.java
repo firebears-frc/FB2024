8f4030c9c37057ed079e42c656254e252a7718fd
Isdogmc <100045710+Isdogmc@users.noreply.github.com>
2024-02-03 15:30:03 -0600

    PID

--- src/main/java/frc/robot/subsystems/UpBeat.java
+++ src/main/java/frc/robot/subsystems/UpBeat.java
@@ -37,17 +37,19 @@ public class UpBeat extends SubsystemBase {
         bottomMotor.setIdleMode(IdleMode.kCoast);
         bottomPid = bottomMotor.getPIDController();
 
-        topPid.setP(0);
-        topPid.setI(0);
+        topPid.setP(0.0003);
+        topPid.setI(0.0000001);
         topPid.setD(0.0);
         topPid.setFF(0.0001875);
+        topPid.setIZone(100);
         topPid.setOutputRange(0.0, 1.0);
         topMotor.burnFlash();
 
-        bottomPid.setP(0);
-        bottomPid.setI(0);
+        bottomPid.setP(0.0003);
+        bottomPid.setI(0.0000001);
         bottomPid.setD(0.0);
         bottomPid.setFF(0.0001875);
+        bottomPid.setIZone(100);
         bottomPid.setOutputRange(0.0, 1.0);
         bottomMotor.burnFlash();
     }
