26ae70162cec0b0cdffd671ee7a2c73a57d2aa74
JJ M <115192983+PineappleGirl5@users.noreply.github.com>
2024-02-08 19:59:15 -0600

    upbeat is now busy untill up to speed (#61)
    * spent way too long rewriting the upbeat
    
    * updated logger
    
    * changed belt sizes to be the same
    
    * did stuff
    
    * deleted line 83

--- src/main/java/frc/robot/subsystems/UpBeat.java
+++ src/main/java/frc/robot/subsystems/UpBeat.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import java.util.ResourceBundle.Control;
 
+import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
 
 import com.revrobotics.CANSparkMax;
@@ -18,6 +19,8 @@ public class UpBeat extends SubsystemBase {
     private SparkPIDController topPid;
     private CANSparkMax bottomMotor;
     private SparkPIDController bottomPid;
+    @AutoLogOutput(key = "upBeat/setPoint")
+    private double setPoint = 0;
 
 
     public UpBeat() {
@@ -54,42 +57,60 @@ public class UpBeat extends SubsystemBase {
         bottomMotor.burnFlash();
     }
 
+    private final static class Constants{
+        private static final double stop = 0.00;
+        private static final double reverse = -1000.00;
+        private static final double shoot = 4000.00;
+        private static final double amp = 1000.00;
+    }
+
+    @AutoLogOutput(key = "upBeat/speed")
+    private double getSpeed() {
+        return bottomMotor.getEncoder().getVelocity();
+    }
+
+    @AutoLogOutput(key = "upBeat/error")
+    private double getError(){
+        return setPoint-getSpeed();
+    }
+
+    @AutoLogOutput(key = "upBeat/at speed")
+    private boolean atSpeed(){
+        return Math.abs(getError()) < 100;
+    }
+
+    private Command speedCommand(double speed){
+        return run (()-> setPoint = speed).until(this::atSpeed); 
+    }
+
     public Command shootNote() {
         return startEnd(
             () -> {
-                topPid.setReference(4800, ControlType.kVelocity);
-                bottomPid.setReference(4000, ControlType.kVelocity);
+                speedCommand(Constants.shoot);
             },
             () -> {
-                topPid.setReference(0, ControlType.kVelocity);
-                bottomPid.setReference(0, ControlType.kVelocity);
+                speedCommand(Constants.stop);
             }
             );
     }
 
     public Command reverseShootNote() {
-        return runOnce(() -> {
-            topPid.setReference(-1000, ControlType.kVelocity);
-            bottomPid.setReference(-1200, ControlType.kVelocity);
-        });
+        return speedCommand(Constants.reverse);
     }
 
     public Command pauseUpBeat() {
-        return runOnce(() -> {
-            topPid.setReference(0, ControlType.kVelocity);
-            bottomPid.setReference(0, ControlType.kVelocity);
-        });
+        return speedCommand(Constants.stop);
     }
 
     public Command ampSpeed() {
-        return runOnce(() -> {
-            topPid.setReference(1000, ControlType.kVelocity);
-            bottomPid.setReference(1200, ControlType.kVelocity);
-        });
+        return speedCommand(Constants.amp);
     }
 
     @Override
     public void periodic() {
+        topPid.setReference(setPoint, ControlType.kVelocity);
+        bottomPid.setReference(setPoint, ControlType.kVelocity);
+
         Logger.recordOutput("upBeat/topOutPut", topMotor.getAppliedOutput());
         Logger.recordOutput("upBeat/ottomOutpt", bottomMotor.getAppliedOutput());
         Logger.recordOutput("upBeat/topSpeed", topMotor.getEncoder().getVelocity());
