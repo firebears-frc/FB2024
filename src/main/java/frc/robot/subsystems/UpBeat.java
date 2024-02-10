0792c9e0c2aa42116221744790b2ad473f5b9826
PineappleGirl5 <bigflatblackhole@gmail.com>
2024-02-09 21:25:45 -0600

    updated vendor deps

--- src/main/java/frc/robot/subsystems/UpBeat.java
+++ src/main/java/frc/robot/subsystems/UpBeat.java
@@ -3,8 +3,6 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import java.util.ResourceBundle.Control;
-
 import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
 
