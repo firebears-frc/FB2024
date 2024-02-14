28d5c5f8c35d0ef96b0124b9f44220a122c75933
Arydragons <sunnybgp.icloud.com>
2024-02-13 20:14:37 -0600

    Added basic loghts code

---
+++ src/main/java/frc/robot/Lights.java
@@ -0,0 +1,25 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Lights extends SubsystemBase{
+    private AddressableLED light_strip;
+    private AddressableLEDBuffer light_stripBuffer;
+    public void robotInit() {
+        light_strip = new AddressableLED(9);
+        light_stripBuffer = new AddressableLEDBuffer(50);
+        light_strip.setLength(light_stripBuffer.getLength());
+        light_strip.setData(light_stripBuffer);
+        light_strip.start();
+        for (var i = 0; i < light_stripBuffer.getLength(); i++) {
+            light_stripBuffer.setHSV(i, 1, 225, 135);
+
+        }
+    }
+    
+}
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+
