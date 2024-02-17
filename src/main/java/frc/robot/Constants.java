915e8644ff4635dd99373c1f76f2256e8ef53c8c
PineappleGirl5 <bigflatblackhole@gmail.com>
2024-02-16 21:28:57 -0600

    changed numbers

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -90,7 +90,7 @@ public class Constants {
         public static final double kTurningEncoderPositionPIDMinInput = 0; // radians
         public static final double kTurningEncoderPositionPIDMaxInput = kTurningEncoderPositionFactor; // radians
 
-        public static final double kDrivingP = 0.1;
+        public static final double kDrivingP = 0.25;
         public static final double kDrivingI = 0;
         public static final double kDrivingD = 0;
         public static final double kDrivingFF = 1 / kDriveWheelFreeSpeedRps;
