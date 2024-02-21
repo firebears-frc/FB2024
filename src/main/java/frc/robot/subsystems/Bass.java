d0215ffdeee70c3472ef09144da9b1aa2d41a21e
PineappleGirl5 <bigflatblackhole@gmail.com>
2024-02-20 20:40:30 -0600

    added stuff to constructor

--- src/main/java/frc/robot/subsystems/Bass.java
+++ src/main/java/frc/robot/subsystems/Bass.java
@@ -1,6 +1,9 @@
 package frc.robot.subsystems;
 
 import org.littletonrobotics.junction.LoggedRobot;
+
+import java.util.List;
+
 import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
 
@@ -9,6 +12,9 @@ import com.pathplanner.lib.util.ReplanningConfig;
 import com.pathplanner.lib.util.PIDConstants;
 import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.GoalEndState;
+import com.pathplanner.lib.path.PathConstraints;
+import com.pathplanner.lib.path.PathPlannerPath;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.SPI;
@@ -16,6 +22,7 @@ import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
@@ -96,6 +103,33 @@ public class Bass extends SubsystemBase {
                 },
                 this // Reference to this subsystem to set requirements
         );
+
+        // Create a list of bezier points from poses. Each pose represents one waypoint.
+        // The rotation component of the pose should be the direction of travel. Do not
+        // use holonomic rotation.
+        List<Translation2d> bezierPoints = PathPlannerPath.bezierFromPoses(
+                new Pose2d(1.0, 1.0, Rotation2d.fromDegrees(0)),
+                new Pose2d(3.0, 1.0, Rotation2d.fromDegrees(0)),
+                new Pose2d(5.0, 3.0, Rotation2d.fromDegrees(90)));
+
+        // Create the path using the bezier points created above
+        PathPlannerPath path = new PathPlannerPath(
+                bezierPoints,
+                new PathConstraints(3.0, 3.0, 2 * Math.PI, 4 * Math.PI), 
+                // The constraints for this path. If using a differential drivetrain, the angularmconstraints have no effect.
+
+                new GoalEndState(0.0, Rotation2d.fromDegrees(-90)) 
+                // Goal end state. You can set a holonomic rotation here. If using a differential drivetrain, the rotation will have no effect.
+        );
+
+        // Prevent the path from being flipped if the coordinates are already correct
+        path.preventFlipping = false;
+    }
+
+    public Command alignToSpeaker(){
+        return runOnce(( -> {
+
+        }));
     }
 
     @Override
