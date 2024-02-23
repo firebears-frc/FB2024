2c6a211d3e8a921d0191bcc088ae4be0a949c8b2
PineappleGirl5 <bigflatblackhole@gmail.com>
2024-02-22 21:06:02 -0600

    lights now turn orange when a note is detected by the beambreak sensor

--- src/main/java/frc/robot/subsystems/Lights.java
+++ src/main/java/frc/robot/subsystems/Lights.java
@@ -6,15 +6,20 @@ import org.littletonrobotics.junction.Logger;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class Lights extends SubsystemBase {
     private AddressableLED light_strip;
     private AddressableLEDBuffer light_stripBuffer;
 
     private boolean isRed;
+    private boolean e = false;
+
+    private DigitalInput sensor;
 
     public Lights() {
         light_strip = new AddressableLED(9);
@@ -33,6 +38,8 @@ public class Lights extends SubsystemBase {
 
         setDefault();
 
+        sensor = new DigitalInput(0);
+
         Logger.recordOutput("Lights/isRedAlliance", isRed);
     }
 
@@ -44,17 +51,37 @@ public class Lights extends SubsystemBase {
         }
     }
 
-    private void setRed() {
+    private void setRed(){
         for (var i = 0; i < light_stripBuffer.getLength(); i++) {
             light_stripBuffer.setRGB(i, 255, 0, 0);
         }
         light_strip.setData(light_stripBuffer);
     }
 
-    private void setBlue() {
+    private void setBlue(){
         for (var i = 0; i < light_stripBuffer.getLength(); i++) {
             light_stripBuffer.setRGB(i, 0, 0, 255);
         }
         light_strip.setData(light_stripBuffer);
+    }
+
+    private void setOrange(){
+        for (var i = 0; i < light_stripBuffer.getLength(); i++) {
+            light_stripBuffer.setRGB(i, 229, 83, 0);
+        }
+        light_strip.setData(light_stripBuffer);
+    }
+
+    @Override
+    public void periodic() {
+        if(sensor.get()){
+            if(!e){
+                setOrange();
+                e = true;
+            }
+        }else if(e){
+            setDefault();
+            e = false;
+        }
     }
 }
