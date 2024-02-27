3ae9fb1db4c9c680992e1e70c971171bee5df727
JJ M <115192983+PineappleGirl5@users.noreply.github.com>
2024-02-26 20:26:51 -0600

    changed spark max to spark flex (#93)
    Co-authored-by: Tyler Seiford <50031864+TylerSeiford@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/MAXSwerveModule.java
+++ src/main/java/frc/robot/subsystems/MAXSwerveModule.java
@@ -11,10 +11,11 @@ import com.revrobotics.CANSparkLowLevel.PeriodicFrame;
 import com.revrobotics.SparkAbsoluteEncoder.Type;
 import com.revrobotics.SparkPIDController;
 import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.CANSparkFlex;
 import com.revrobotics.RelativeEncoder;
 
 public class MAXSwerveModule {
-    private final CANSparkMax m_drivingSparkMax;
+    private final CANSparkFlex m_drivingSparkMax;
     private final CANSparkMax m_turningSparkMax;
 
     private final RelativeEncoder m_drivingEncoder;
@@ -33,7 +34,7 @@ public class MAXSwerveModule {
      * Encoder.
      */
     public MAXSwerveModule(int drivingCANId, int turningCANId, double chassisAngularOffset) {
-        m_drivingSparkMax = new CANSparkMax(drivingCANId, MotorType.kBrushless);
+        m_drivingSparkMax = new CANSparkFlex(drivingCANId, MotorType.kBrushless);
         m_turningSparkMax = new CANSparkMax(turningCANId, MotorType.kBrushless);
 
         // Factory reset, so we get the SPARKS MAX to a known state before configuring
