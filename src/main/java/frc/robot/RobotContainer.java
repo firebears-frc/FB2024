3a5c3d404aee9d094e352721ea1933e6173535d7
PineappleGirl5 <bigflatblackhole@gmail.com>
2024-03-01 09:00:33 -0600

    changed climb buttons

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -106,9 +106,9 @@ public class RobotContainer {
         two.button(6).onTrue(m_climb.climbLeftUp()).onFalse(m_climb.pauseClimb());
         two.button(7).onTrue(m_climb.climb()).onFalse(m_climb.pauseClimb());
 
-        two.button(8).onTrue(m_climb.climbRightDown()).onFalse(m_climb.pauseClimb());
+        two.button(10).onTrue(m_climb.climbRightDown()).onFalse(m_climb.pauseClimb());
         two.button(9).onTrue(m_climb.climbLeftDown()).onFalse(m_climb.pauseClimb());
-        two.button(10).onTrue(m_climb.unclimb()).onFalse(m_climb.pauseClimb());
+        two.button(8).onTrue(m_climb.unclimb()).onFalse(m_climb.pauseClimb());
 
         xboxController.a().onTrue(m_intake.intakeNote()).onFalse(m_intake.pauseDownBeat());
         xboxController.x().onTrue(m_intake.dischargeNote()).onFalse(m_intake.pauseDownBeat());
