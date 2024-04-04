153bcfdffe9c0c052256bfdb3879212cbec9d40f
Isdogmc <100045710+Isdogmc@users.noreply.github.com>
2024-04-03 20:11:52 -0500

    removed auto intake for sequence

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -51,7 +51,7 @@ public class RobotContainer {
             "intake",Commands.parallel(
                     m_shooter.pauseUpBeat(),
                     m_arm.pickUp(),
-                    m_intake.autoIntake()),
+                    m_intake.intakeNote()),
             "armUp",Commands.parallel(
                     m_arm.straightShot(),
                     m_shooter.autoShoot()),
