432a0b418b0ef86c5ed418d8ae386e9b6913afd0
Tyler Seiford <50031864+TylerSeiford@users.noreply.github.com>
2024-04-13 18:41:37 -0500

    Start writing the arm

---
+++ src/main/java/com/seiford/subsystems/arm/ArmIO.java
@@ -0,0 +1,26 @@
+package com.seiford.subsystems.arm;
+
+import org.littletonrobotics.junction.AutoLog;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+
+public interface ArmIO {
+  @AutoLog
+  public static class ArmIOInputs {
+    public Rotation2d positionRadians = new Rotation2d();
+    public double[] appliedVolts = new double[] {};
+    public double[] currentAmps = new double[] {};
+  }
+
+  /** Updates the set of loggable inputs. */
+  public default void updateInputs(ArmIOInputs inputs) {
+  }
+
+  /** Run closed loop to the specified velocity. */
+  public default void setPosition(Rotation2d angle, double ffVolts) {
+  }
+
+  /** Set velocity PID constants. */
+  public default void configurePID(double kP, double kI, double kD) {
+  }
+}
---
+++ src/main/java/com/seiford/subsystems/arm/ArmIOSparkMax.java
@@ -0,0 +1,67 @@
+package com.seiford.subsystems.arm;
+
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.SparkPIDController.ArbFFUnits;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkAbsoluteEncoder;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkBase.ControlType;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+
+public class ArmIOSparkMax implements ArmIO {
+  private final CANSparkMax leader = new CANSparkMax(13, MotorType.kBrushless);
+  private final CANSparkMax follower = new CANSparkMax(12, MotorType.kBrushless);
+  private final SparkAbsoluteEncoder encoder = leader.getAbsoluteEncoder();
+  private final SparkPIDController pid = leader.getPIDController();
+
+  public ArmIOSparkMax() {
+    leader.restoreFactoryDefaults();
+    follower.restoreFactoryDefaults();
+
+    leader.setCANTimeout(250);
+    follower.setCANTimeout(250);
+
+    leader.setInverted(false);
+    follower.follow(leader, true);
+
+    leader.enableVoltageCompensation(12.0);
+    leader.setSmartCurrentLimit(50, 30);
+    leader.setSecondaryCurrentLimit(60.0);
+    follower.enableVoltageCompensation(12.0);
+    follower.setSmartCurrentLimit(50, 30);
+    follower.setSecondaryCurrentLimit(60.0);
+
+    leader.burnFlash();
+    follower.burnFlash();
+  }
+
+  @Override
+  public void updateInputs(ArmIOInputs inputs) {
+    inputs.positionRadians = Rotation2d.fromRotations(encoder.getPosition());
+    inputs.appliedVolts = new double[] {
+      leader.getAppliedOutput() * leader.getBusVoltage(),
+      follower.getAppliedOutput() * follower.getBusVoltage()
+    };
+    inputs.currentAmps = new double[] { leader.getOutputCurrent(), follower.getOutputCurrent() };
+  }
+
+  @Override
+  public void setPosition(Rotation2d angle, double ffVolts) {
+    pid.setReference(
+      angle.getRotations(),
+      ControlType.kPosition,
+      0,
+      ffVolts,
+      ArbFFUnits.kVoltage
+    );
+  }
+  
+  @Override
+  public void configurePID(double kP, double kI, double kD) {
+    pid.setP(kP, 0);
+    pid.setI(kI, 0);
+    pid.setD(kD, 0);
+    pid.setFF(0.0, 0);
+  }
+}
